2ee3bb3ba6fce4df4f6ae5fc8e6ffa6000e87617
Noah Roderick <noaheroderick@gmail.com>
2025-08-31 19:35:52 -0500

    idk codex update

--- scripts/Building.cs
+++ scripts/Building.cs
@@ -2,16 +2,16 @@ using Godot;
 
 public partial class Building : Node
 {
-    [Export] public bool IsOn { get; private set; } = true;
-    [Export] public float PowerCost { get; set; } = 0f;
+	[Export] public bool IsOn { get; private set; } = true;
+	[Export] public float PowerCost { get; set; } = 0f;
 
-    public void Toggle()
-    {
-        IsOn = !IsOn;
-    }
+	public void Toggle()
+	{
+		IsOn = !IsOn;
+	}
 
-    public void SetOn(bool on)
-    {
-        IsOn = on;
-    }
+	public void SetOn(bool on)
+	{
+		IsOn = on;
+	}
 }
